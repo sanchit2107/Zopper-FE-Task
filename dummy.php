c279ce324a2e55dcb1faa9577f09578e1ee98112
sanchit2107 <sanchit2107@gmail.com>
2020-09-07 01:46:40 +0530

    Make changes in Submitting the data | Make Validations more stronger

--- dummy.php
+++ dummy.php
@@ -1,3 +1,20 @@
 <?php
 
-var_dump($_POST);
+    // print_r($_POST);
+
+    $name = $_POST['name'];
+    $searchBy = $_POST['searchBy'];
+    $addedElement = $_POST['addedElement'];
+    
+    if($searchBy == 'phoneno'){
+        $response['error'] = false;
+        $response['message'] = 'This runs by phoneno - '.$addedElement;
+    }
+    else{
+        $response['error'] = false;
+        $response['message'] = 'This runs by email - '.$addedElement;
+    }
+
+    echo json_encode($response); 
+
+?>
--- script.js
+++ script.js
@@ -2,25 +2,43 @@ function addEmail() {
 		$('#inputValue').attr('disabled',false);
 		$('#inputValue').attr('type', 'email');
 		$('#inputValue').attr('placeholder', 'Enter email here');	
+		$('#inputValue').removeAttr('maxlength', '10');
+		$('#inputValue').removeAttr('minlength', '10');
+		$('#inputValue').removeAttr('onkeypress', 'return (event.charCode == 8 || event.charCode == 0 || event.charCode == 13) ? null : event.charCode >= 48 && event.charCode <= 57');
 }
 
 function addPhone() {
-		$('#inputValue').attr('pattern', "[0-9]{10}");
 		$('#inputValue').attr('disabled',false);
-		$('#inputValue').attr('type', 'number');
+		$('#inputValue').attr('type', 'text');
+		$('#inputValue').attr('onkeypress', 'return (event.charCode == 8 || event.charCode == 0 || event.charCode == 13) ? null : event.charCode >= 48 && event.charCode <= 57');
+		$('#inputValue').attr('maxlength', '10');
+		$('#inputValue').attr('minlength', '10');
 		$('#inputValue').attr('placeholder', 'Enter Phone no. here');
 }
 
-// Check length of Mobile Number
-function phonenumber() {
-
-	if($('input[name=searchBy]:checked').val() == "phoneno") {
-		var name = $('#inputValue').val();
-		if (name.length < 10 || name.length > 10)
-		{
-		    alert('Mobile number should be of 10 digits '); 
+// Submitting Form Data
+$('form#myForm').submit(function(e){
+	e.preventDefault();
+	$('input[name="submit-button"]').val('Submitting...');
+	$('input[name="submit-button"]').css('pointer-events', 'none');
+	var formdata = $(this).serialize();
+	console.log(formdata);
+	$.ajax({
+		'type' : 'post',
+		'data' : formdata,
+		'url' : 'dummy.php',
+		success : function(response){
+			var json = JSON.parse(response);
+			if(!json.error){
+				window.alert(json.message);
+			}
+			else{
+				window.alert(json.message);
+			}	
+			$('input[name="submit-button"]').val('Submit Form');
+			$('input[name="submit-button"]').css('pointer-events', 'auto');
 		}
-	}
-} 
+	});
+});
 
 
